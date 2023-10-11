07efa5ec3f3a125c84f80698b8f873479987462e
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-10-11 00:03:55 -0700

    record relevant rainfall metrics

--- src/village_simulator/simulation/components/resources.py
+++ src/village_simulator/simulation/components/resources.py
@@ -187,11 +187,17 @@ class Wheat(Resource):
 
     @property
     def columns_created(self) -> List[str]:
-        return super().columns_created + [self.projected_yield_column]
+        return super().columns_created + [
+            self.projected_yield_column,
+            self.previously_dry_column,
+            self.cumulative_dry_days_column,
+            self.rainfall_mid_growth_column,
+            self.rainfall_late_growth_column,
+        ]
 
     @property
     def columns_required(self) -> Optional[List[str]]:
-        return super().columns_required + ["temperature"]
+        return super().columns_required + ["temperature", "rainfall"]
 
     #####################
     # Lifecycle methods #
@@ -200,13 +206,24 @@ class Wheat(Resource):
     def __init__(self):
         super().__init__("wheat")
         self.projected_yield_column = f"projected_{self.resource}_yield"
+        self.previously_dry_column = "previous_day_dry"
+        self.cumulative_dry_days_column = "cumulative_dry_days"
+        self.rainfall_mid_growth_column = "rainfall_mid_growth"
+        self.rainfall_late_growth_column = "rainfall_late_growth"
 
     def setup(self, builder: Builder) -> None:
         super().setup(builder)
         self.clock = builder.time.clock()
         self.step_size = builder.time.step_size()
+
+        start_date = get_time_stamp(builder.configuration.time.start)
+        self.next_sowing_date = get_next_annual_event_date(
+            start_date,
+            self.configuration.sowing_date.month,
+            self.configuration.sowing_date.day,
+        )
         self.next_harvest_date = get_next_annual_event_date(
-            get_time_stamp(builder.configuration.time.start),
+            start_date,
             self.configuration.harvest_date.month,
             self.configuration.harvest_date.day,
         )
@@ -223,55 +240,88 @@ class Wheat(Resource):
 
         feature = self.population_view.subview([FEATURE]).get(pop_data.index)
         village_index = feature[feature == "village"].index
-        expected_wheat_yield = pd.Series(
-            0.0, index=pop_data.index, name=self.projected_yield_column
+        created_columns = pd.DataFrame(
+            {
+                self.projected_yield_column: 0.0,
+                self.previously_dry_column: False,
+                self.cumulative_dry_days_column: 0.0,
+                self.rainfall_mid_growth_column: 0.0,
+                self.rainfall_late_growth_column: 0.0,
+            },
+            index=pop_data.index,
         )
-        expected_wheat_yield[village_index] = (
+        created_columns.loc[village_index, self.projected_yield_column] = (
             self.initial_village_size * self.configuration.annual_per_capita_accumulation.loc
         )
-        self.population_view.update(expected_wheat_yield)
+        self.population_view.update(created_columns)
 
     def on_time_step(self, event: Event) -> None:
         """
-        Modify the expected wheat yield based on the current temperature
+        Modify the expected wheat yield based on the current temperature.
+        Track rainfall metrics.
         """
-        super().on_time_step(event)
-
-        clock_time = self.clock()
-        next_harvest_date = get_next_annual_event_date(
-            clock_time,
-            self.configuration.harvest_date.month,
-            self.configuration.harvest_date.day,
-        )
-        next_sowing_date = get_next_annual_event_date(
-            clock_time,
-            self.configuration.sowing_date.month,
-            self.configuration.sowing_date.day,
-        )
-
         # if next harvest date is less than the next sowing date, update projected yield
-        if next_harvest_date < next_sowing_date:
-            expected_yield = self.population_view.get(event.index)[
-                self.projected_yield_column
-            ]
-            effect_of_temperature = self.effect_of_temperature_on_yield(expected_yield.index)
-            expected_yield *= effect_of_temperature
-            self.population_view.update(expected_yield)
+        if self.next_harvest_date < self.next_sowing_date:
+            data = self.population_view.get(event.index)
+            effect_of_temperature = self.effect_of_temperature_on_yield(data.index)
+            data[self.projected_yield_column] *= effect_of_temperature
+
+            updates = pd.DataFrame(data[self.projected_yield_column])
+
+            clock_time = self.clock()
+            if clock_time > self.next_harvest_date - pd.Timedelta(days=90):
+                updates[self.rainfall_late_growth_column] = (
+                    data[self.rainfall_late_growth_column] + data["rainfall"]
+                )
+            elif clock_time > self.next_harvest_date - pd.Timedelta(days=180):
+                updates[self.rainfall_mid_growth_column] = (
+                    data[self.rainfall_mid_growth_column] + data["rainfall"]
+                )
+
+            cumulatively_dry = data[self.previously_dry_column] & (data["rainfall"] == 0.0)
+            updates[self.cumulative_dry_days_column] = (
+                data[self.cumulative_dry_days_column] + cumulatively_dry
+            )
+
+            self.population_view.update(updates)
+
+        # Update stores
+        super().on_time_step(event)
 
     def on_time_step_cleanup(self, event: Event) -> None:
         """
-        Reset the expected yield after harvest has been done
+        Sets the previously dry day column. Updates the next sowing date if
+        sowing occurred during this time-step. Resets the all yield related
+        columns and updates the next harvest date if the harvest occurred during
+        this time-step.
         """
+
+        rainfall = self.population_view.get(event.index)["rainfall"]
+        updates = pd.DataFrame({self.previously_dry_column: rainfall == 0.0})
+
         clock_time = self.clock()
+        if clock_time < self.next_sowing_date <= clock_time + self.step_size():
+            self.next_sowing_date += ONE_YEAR
+
         if clock_time < self.next_harvest_date <= clock_time + self.step_size():
+            self.next_harvest_date += ONE_YEAR
+
             expected_yield = self.population_view.get(event.index)[
                 self.projected_yield_column
             ]
-            expected_yield = (
+            updates[self.projected_yield_column] = (
                 self.total_population(expected_yield.index)
                 * self.configuration.annual_per_capita_accumulation.loc
-            ).rename(self.projected_yield_column)
-            self.population_view.update(expected_yield)
+            )
+            updates[
+                [
+                    self.cumulative_dry_days_column,
+                    self.rainfall_mid_growth_column,
+                    self.rainfall_late_growth_column,
+                ]
+            ] = 0.0
+
+        self.population_view.update(updates)
 
     #################
     # Setup methods #
@@ -301,7 +351,6 @@ class Wheat(Resource):
         clock_time = self.clock()
         if clock_time < self.next_harvest_date <= clock_time + self.step_size():
             harvest_quantity = self.population_view.get(index)[self.projected_yield_column]
-            self.next_harvest_date += ONE_YEAR
         else:
             harvest_quantity = pd.Series(
                 0.0, index=index, name=f"{self.resource}.accumulation"
